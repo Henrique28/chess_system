4617f3a97042e109495dc782cb2efaf36dda282c
Henrique Vieira Nascimento <henrique.vieira2820@gmail.com>
2025-07-01 15:02:24 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardlayer.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello word");
-
+		Position p = new Position(5, 10); 
+		System.out.println(p);
 	}
 
 }
